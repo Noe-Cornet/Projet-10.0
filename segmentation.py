fb929425335ad382a2c697e69c20b172f9506d8e
eliasnacef21 <165177410+eliasnacef21@users.noreply.github.com>
2024-04-10 19:02:48 -0400

    Add files via upload

--- segmentation.py
+++ segmentation.py
@@ -1,17 +1,91 @@
-from manupulation_histogramme import calculer_distance_1
-import random
-def regrouper_points(data,k,max_iteration):
+import math
+import numpy as np
 
-    # tirage aléatoire des points
+def regrouper_points(data,k,max_iterations=50):
 
-    points_random=[]
-    nb_lignes_data,nb_colonne_data=data.shape
 
-    # boucle i permet de selections k points de façon aléatoire
-    while len(points_random) != k:
-        f=[random.randint(0,nb_lignes_data),random.randint(0,nb_colonne_data)]
-        print(f)
-        # verification que le point n'a pas deja été tiré au sort
-        if f not in points_random:
-            points_random.append(f)
-    # points_randome est un de kx2 chaque ligne corresponds à une coordoné de points 
+    indice_histo = {}
+    for i in range(len(data)):
+        indice_histo[data[i]] = i
+
+
+    dict = {}
+
+
+    for i in range(k):
+        dict[data[i]]=[]
+    for iterations in range(max_iterations):
+        for i in range(data):
+            min_distance = None
+            nearest_point = None
+            point_courant = data[i]
+            for centre in dict:
+
+                distance = calculer_distance_1(centre, point_courant)
+
+                if min_distance == None:
+                    min_distance = distance
+                    nearest_point = centre
+
+                elif distance < min_distance:
+                    min_distance = distance
+                    nearest_point = centre
+
+
+            dict[nearest_point].append(point_courant)
+
+            nouveau_dict = {}
+
+        for cle in dict:
+            centre_groupe_points = dict[cle]
+
+
+            moyennes = []
+            nb_colonnes = cle.shape[0]
+
+            for dim in range(nb_colonnes):
+                moyenne_dim = sum(p[dim] for p in centre_groupe_points) / len(centre_groupe_points)
+                moyennes.append(moyenne_dim)
+
+            nouveau_dict[moyennes] = centre_groupe_points
+        
+        dict = nouveau_dict
+
+    output = np.zeros(len(data))
+
+    indice_groupe = 0
+
+    for cle in dict:
+        centre_groupe_points = dict[cle]
+        for j in range(len(centre_groupe_points)):
+            point_courant = centre_groupe_points[j]
+
+            point_courant_indice = indice_histo[point_courant]
+            output[point_courant_indice] = indice_groupe
+        indice_groupe += 1
+    return output
+
+
+
+
+
+
+def calculer_distance_1(histogramme1, histogramme2):
+    distance = 0
+    colonne = histogramme1.shape
+
+    # soustraction de l'histogramme 1 par le 2 en tout en les parcourants
+    for i in range(colonne[0]):
+        r = float(histogramme1[i]) - float(histogramme2[i])
+        distance = distance + r ** 2
+
+    # arrondi la distance à 2 chiffre après la virgule
+
+    distance = round(distance ** 0.5, 2)
+    return distance
+
+
+
+
+
+
---
+++ test_appliquer_transformation_2_a.py
@@ -0,0 +1,30 @@
+import unittest
+import numpy as np
+from appliquer_transformation_2 import appliquer_transformation_2
+
+class TestAppliquerTransformation2(unittest.TestCase):
+
+    def test_appliquer_transformation_2_a(self):
+        image_gris_test = np.array([
+            [88, 102, 133, 49, 145, 123],
+            [14, 100, 200, 121, 55, 56],
+            [40, 101, 92, 100, 99, 30],
+            [255, 23, 155, 88, 12, 78],
+            [100, 200, 23, 82, 155, 254]
+        ], dtype=np.uint8)
+
+        rayon = 1
+        tableau_attendu = np.array([
+            [0, 0, 0, 0, 0, 0],
+            [0, 3, 6, 3,6, 0],
+            [0, 6, 5, 3, 5, 0],
+            [0, 7, 5, 3, 6, 0],
+            [0, 0, 0, 0, 0, 0]
+        ], dtype=np.uint8)  # Vous devez définir manuellement les valeurs attendues
+        tableau_obtenu = appliquer_transformation_2(image_gris_test, rayon)
+
+        self.assertTrue(np.array_equal(tableau_obtenu, tableau_attendu),
+                        "Le tableau obtenu ne correspond pas au tableau attendu.")
+
+if __name__ == '__main__':
+    unittest.main()
